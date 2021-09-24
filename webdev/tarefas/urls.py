f955981b42834fa582cfcc13464403cf00c3aca2
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-09-24 00:29:35 -0300

    Criado botão apagar tarefa e implementado listagem de tarefas feitas
    close #7
    close #8

--- webdev/tarefas/urls.py
+++ webdev/tarefas/urls.py
@@ -5,6 +5,7 @@ app_name = 'tarefas'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:tarefa_id>', views.detalhe, name='detalhe')
+    path('<int:tarefa_id>', views.detalhe, name='detalhe'),
+    path('apagar/<int:tarefa_id>', views.apagar, name='apagar')
 
 ]
